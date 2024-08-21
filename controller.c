c84b5534e111da272e698b7b971a8389881b5a13
s2c2 <gabriel.sadigursky@gmail.com>
2024-08-21 15:46:45 -0300

    tirei o filtro do controlador

--- controller.c
+++ controller.c
@@ -61,10 +61,10 @@ void *start_controller()
 
             double Ang_saida = asin(Sinal_controle) * (200 / M_PI);
             Ang_saida = round(Ang_saida * 100.0) / 100.0;
-            if(fabs(sin(M_PI / 2 * Ang_saida  / 100) - sin(M_PI / 2 * Ang_saida_anterior / 100)) > 0.2)
+            /*if(fabs(sin(M_PI / 2 * Ang_saida  / 100) - sin(M_PI / 2 * Ang_saida_anterior / 100)) > 0.2)
             {
                 Ang_saida = Ang_saida_anterior;
-            }
+            }*/
             double delta;
 
                 delta = Ang_saida - Ang_saida_anterior;
